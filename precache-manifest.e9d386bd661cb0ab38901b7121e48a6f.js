75ddf6f0dd24465826ac74a3570cb98c512def98
fheyen <frank.heyen@outlook.de>
2020-10-15 18:38:06 +0200

    Updates

---
+++ precache-manifest.e9d386bd661cb0ab38901b7121e48a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc403a0e844ddef81ed821fafcb18ce6",
+    "url": "/midi-live-vis/index.html"
+  },
+  {
+    "revision": "e4c20bfee4c5e7b818d5",
+    "url": "/midi-live-vis/static/css/main.03dff141.chunk.css"
+  },
+  {
+    "revision": "3c3d209bb5d640b8d92b",
+    "url": "/midi-live-vis/static/js/2.0d3261a2.chunk.js"
+  },
+  {
+    "revision": "0cdebb9bd8921e6ffed16c110c74fb19",
+    "url": "/midi-live-vis/static/js/2.0d3261a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4c20bfee4c5e7b818d5",
+    "url": "/midi-live-vis/static/js/main.8db7971a.chunk.js"
+  },
+  {
+    "revision": "49aec70c64e560e15e15",
+    "url": "/midi-live-vis/static/js/runtime-main.a0344f4c.js"
+  }
+]);
